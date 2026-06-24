c7f5136960c785000f4b899461580ee64b54c14b
khyatikumar <anuradhamaruvada@gmail.com>
2026-06-24 19:30:21 +0530

    Groq problem solved

--- app/main.py
+++ app/main.py
@@ -61,11 +61,17 @@ app = FastAPI(
 from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
+    # CORSMiddleware,
+    # allow_origins=[
+    #     "https://mednexus360.vercel.app",
+    #     "http://localhost:5173"
+    # ],
+    # allow_credentials=True,
+    # allow_methods=["*"],
+    # allow_headers=["*"],
+    
     CORSMiddleware,
-    allow_origins=[
-        "https://mednexus360.vercel.app",
-        "http://localhost:5173"
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
