2540f8fd72b4aef50e02a47ce25bcdd53ec801e9
khyatikumar <anuradhamaruvada@gmail.com>
2026-06-30 14:26:54 +0530

    Switch  to Mistral

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
-    GROQ_API_KEY:str
+ MISTRAL_API_KEY:str
 
     class Config:
         env_file = ".env"
